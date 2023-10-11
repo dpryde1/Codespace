e032b204765643b6ee858645fe51d7982e033ce5
David Pryde <dpryde@live.co.uk>
2023-10-11 09:53:58 +0100

    11oct0953

--- Testing_Technologies/Secret_Number/secretnumber.js
+++ Testing_Technologies/Secret_Number/secretnumber.js
@@ -16,24 +16,14 @@ buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (attempts > 0) {
       attempts--; // Decrements the number of attempts
-      console.log(`Attempts: ${attempts}`); // Just for debug (delete after)
 
       const playerChoice = parseInt(button.id); // looks at the button's ID as an integer
-
-      //const computer = computerChoice(); // Can't be declared here otherwise a new number is generated everytime a button is pressed
-
-      console.log(`Computer number: ${computer}`); // Just for debug (delete after)
 
       if (playerChoice === computer) {
         resultDiv.textContent = `That's right, you win! The correct answer was ${computer}.`;
       } else if (playerChoice < computer) {
         resultDiv.textContent = `You guessed too low! You have ${attempts} attempts left.`;
-      }
-      // It can't be like this because we have only 3 options an 'else' statement should be used instead
-      // } else if (playerChoice > computer) {
-      //   resultDiv.textContent = `You guessed too high! You have ${attempts} attempts left.`;
-      // }
-      else {
+      } else {
         resultDiv.textContent = `You guessed too high! You have ${attempts} attempts left.`;
       }
 
