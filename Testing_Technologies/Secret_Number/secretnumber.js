99cea3b7a3b21ce3851f7fa2af19eed0ac1ad98e
David Pryde <dpryde@live.co.uk>
2023-10-11 09:15:14 +0100

    11oct

--- Testing_Technologies/Secret_Number/secretnumber.js
+++ Testing_Technologies/Secret_Number/secretnumber.js
@@ -9,18 +9,32 @@ let attempts = 3; // Number of attempts the player has
 const buttons = document.querySelectorAll("button");
 const resultDiv = document.getElementById("result");
 
+// Generate the computer's choice
+const computer = computerChoice();
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (attempts > 0) {
-      const playerChoice = parseInt(button.id); // Parse the button's ID as an integer
-      const computer = computerChoice();
+      attempts--; // Decrements the number of attempts
+      console.log(`Attempts: ${attempts}`); // Just for debug (delete after)
+
+      const playerChoice = parseInt(button.id); // looks at the button's ID as an integer
+
+      //const computer = computerChoice(); // Can't be declared here otherwise a new number is generated everytime a button is pressed
+
+      console.log(`Computer number: ${computer}`); // Just for debug (delete after)
 
       if (playerChoice === computer) {
         resultDiv.textContent = `That's right, you win! The correct answer was ${computer}.`;
       } else if (playerChoice < computer) {
-        resultDiv.textContent = `You guessed too low! You have ${--attempts} attempts left.`;
-      } else if (playerChoice > computer) {
-        resultDiv.textContent = `You guessed too high! You have ${--attempts} attempts left.`;
+        resultDiv.textContent = `You guessed too low! You have ${attempts} attempts left.`;
+      }
+      // It can't be like this because we have only 3 options an 'else' statement should be used instead
+      // } else if (playerChoice > computer) {
+      //   resultDiv.textContent = `You guessed too high! You have ${attempts} attempts left.`;
+      // }
+      else {
+        resultDiv.textContent = `You guessed too high! You have ${attempts} attempts left.`;
       }
 
       if (attempts === 0 && playerChoice !== computer) {
